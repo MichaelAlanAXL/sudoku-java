14d2604c8aac8673bf9a33724f9d34cd395a179a
MichaelAlanAXL <michael.dtna@gmail.com>
2025-09-10 11:26:44 -0300

    feat: mini Sudoku 3x3 with number validation and no duplicates

--- src/src/main/java/dev/michael/sudoku/App.java
+++ src/src/main/java/dev/michael/sudoku/App.java
@@ -6,24 +6,54 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Mini Sudoku 2x2");
+        JFrame frame = new JFrame("Mini Sudoku 3x3");
+        JPanel panel = new JPanel(new GridLayout(3,3,5,5));
         
-        // grid 2 x 2
-        JPanel panel = new JPanel(new GridLayout(2,2,5,5));
+        JTextField[] cells = new JTextField[9];
 
-        JTextField cell1 = new JTextField();
-        JTextField cell2 = new JTextField();
-        JTextField cell3 = new JTextField();
-        JTextField cell4 = new JTextField();
+        for (int i = 0; i < 9; i++) {
+            cells[i] = new JTextField();
 
-        panel.add(cell1);
-        panel.add(cell2);
-        panel.add(cell3);
-        panel.add(cell4);
+            // Validação
+            cells[i].setInputVerifier(new InputVerifier() {
+                @Override
+                public boolean verify(JComponent input) {
+                    JTextField field = (JTextField) input;
+                    String text = field.getText();
+
+                    try {
+                        int value = Integer.parseInt(text);
+
+                        // validar números
+                        if (value < 1 || value > 9) {
+                            JOptionPane.showMessageDialog(frame, "Digite apenas números de 1 a 9!");
+                            field.setText("");
+                            return false;
+                        }
+                        // validar números repetidos
+                        for (JTextField other : cells) {
+                            if (other != field && text.equals(other.getText())) {
+                                JOptionPane.showMessageDialog(frame, "Número já usado!");
+                                field.setText("");
+                                return false;
+                            }
+                        }
+
+
+                    } catch (NumberFormatException e) {
+                        JOptionPane.showMessageDialog(frame, "Digite apenas números!");
+                        field.setText("");
+                        return false;
+                    }
+                    return true;
+                }
+            });
+
+            panel.add(cells[i]);            
+        }
 
         frame.add(panel);
-
-        frame.setSize(300, 300);
+        frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
